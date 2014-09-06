cfe2c68ce3de5660e21f5d1555f334f2671ead3a
Luke Policinski <lpolicinski@gmail.com>
2014-09-06 14:46:11 +0000

    removing http from font

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -12,7 +12,7 @@
         <title><?php echo $title?></title>
         
         <!-- #GOOGLE FONT -->
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
+	<link rel="stylesheet" href=//fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,300,400,700">
         <?php
             // CSS FILES
             Casset::css('bootstrap.min.css');
