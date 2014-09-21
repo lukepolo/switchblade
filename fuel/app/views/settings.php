f85a04619d9ad7896546693e3df69007bce9efb5
Luke Policinski <lpolicinski@gmail.com>
2014-09-21 16:47:33 +0000

    making the settings look alittle better

--- fuel/app/views/settings.php
+++ fuel/app/views/settings.php
@@ -1,4 +1,4 @@
-<div class="smart-form">
+<div class="well well-light well-sm .smart-form">
     <section class="col col-5">
         <h1>Edit Site Settings</h1>
         <?php
@@ -9,32 +9,49 @@
             ));
             foreach($settings as $setting)
             {
-                switch($setting->type)
-                {
-                    case 'varchar':
-                        echo \Form::label(ucwords(str_replace('_',' ', $setting->name)));
-                        echo \Form::input($setting->name, $setting->data);
-                    break;
-                    case 'boolean': 
-                    ?>
-                        <label class="toggle">
-                            <?php 
-                                echo \Form::input($setting->name, "false", array('type' => 'hidden'));
-                                
-                                if($setting->data == 'false')
-                                {
-                                    $setting->data = false;
-                                }
-                                echo \Form::checkbox($setting->name, $setting->data, $setting->data, array('toggle' => true));
-                                echo ucwords($setting->name);
-                            ;?>
-                        </label>
-                    <?php
-                    break;
-                }
+            ?>
+                        <?php
+                        switch($setting->type)
+                        {
+                            case 'varchar':
+                            ?>
+                                <section>
+                                <label class="input"> 
+                            <?php
+                                echo \Form::label(ucwords(str_replace('_',' ', $setting->name)));
+                                echo \Form::input($setting->name, $setting->data);
+                            break;
+                            case 'boolean': 
+                            ?>
+                                <section class="col-2">
+                                <label class="input"> 
+                                <label class="toggle">
+                                    <?php 
+                                        echo \Form::input($setting->name, "false", array('type' => 'hidden'));
+
+                                        if($setting->data == 'false')
+                                        {
+                                            $setting->data = false;
+                                        }
+                                        echo \Form::checkbox($setting->name, $setting->data, $setting->data, array('toggle' => true));
+                                        echo ucwords($setting->name);
+                                    ;?>
+                                </label>
+                            <?php
+                            break;
+                        }
+                        ?>
+                    </label>
+                </section>
+                <?php
             }
-            echo \Form::button('Update Settings', null, array('class' => 'btn btn-primary'));
-            echo \Form::close();
-        ?>
+            ?>
+            <br>
+            <footer>
+                <?php
+                    echo \Form::button('Update Settings', null, array('class' => 'btn btn-primary'));
+                    echo \Form::close();
+                ?>
+            </footer>
     </section>
 </div>
