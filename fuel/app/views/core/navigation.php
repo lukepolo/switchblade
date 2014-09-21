86b288f1fcb029eb20ee6ffaa7c6f5d196b10345
Luke Policinski <lpolicinski@gmail.com>
2014-09-21 02:46:01 +0000

    added proper logout button

--- fuel/app/views/core/navigation.php
+++ fuel/app/views/core/navigation.php
@@ -6,7 +6,7 @@
             <a href="javascript:void(0);" id="show-shortcut" data-action="toggleShortcut">
                 <img src="/assets/img/avatars/sunny.png" alt="me" class="online" />
                 <span>
-                    john.doe
+                    <?php echo Auth::get('first_name'); ?>
                 </span>
                 <i class="fa fa-angle-down"></i>
             </a>
