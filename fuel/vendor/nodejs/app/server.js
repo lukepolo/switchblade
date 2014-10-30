9b6029dd99740ad974729d4384045d5b3a5efdff
Luke Policinski <lpolicinski@gmail.com>
2014-10-30 20:14:00 +0000

    Added PHP To Node, elephant.io

--- fuel/vendor/nodejs/app/server.js
+++ fuel/vendor/nodejs/app/server.js
@@ -7,18 +7,15 @@ var os = require('os')
 var https = require('https');
 fs = require('fs');
 var options = {
-  key:    fs.readFileSync('/etc/ssl/switchblade.key'),
-  cert:   fs.readFileSync('/etc/ssl/switchblade.crt'),
-  ca:     fs.readFileSync('/etc/ssl/COMODORSADomainValidationSecureServerCA.crt')
+    key:    fs.readFileSync('/etc/ssl/switchblade.key'),
+    cert:   fs.readFileSync('/etc/ssl/switchblade.crt'),
+    ca:     fs.readFileSync('/etc/ssl/COMODORSADomainValidationSecureServerCA.crt')
 };
+
 var server = require('https').createServer(options);
 var io = require('socket.io')(server);
 
 server.listen(7777);
-
-// TODO
-// SET IS DEPRECATED 
-
 
 // Maintains a list of timeouts
 var offline_timeout = {};
@@ -26,82 +23,86 @@ var offline_timeout = {};
 // Maintains an active list of users
 var users = {};
 
-
-
 // We need to make sure they have a session, otherwise they are not allowed to access node!
 io.use(function(socket, next) 
 {
-  // Get all the interfaces
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-  // Go through all the interfaces
-  for (k in interfaces) {
-      for (k2 in interfaces[k]) {
-          var address = interfaces[k][k2];
-          // Check to see if we have an IPv4 address
-          if (address.family == 'IPv4' && !address.internal) {
-              // Record the address
-              addresses.push(address.address)
-          }
-      }
-  }
+    // Get all the interfaces
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    // Go through all the interfaces
+    for (k in interfaces) {
+        for (k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            // Check to see if we have an IPv4 address
+            if (address.family == 'IPv4' && !address.internal) {
+                // Record the address
+                addresses.push(address.address)
+            }
+        }
+    }
   
-  // Check to see if the request is coming from our server, via PHP
-  if(addresses.indexOf(socket.request.connection.remoteAddress) >= 0)
-  {
-    next();
-  }
-  else
-  {
-    // Get the cookies from the headers
-    var cookies = cookie.parse(socket.request.headers.cookie);
-    
-    var client = new memcache.Client(11211, "127.0.0.1");
-    client.connect();
-    
-    var session = unserialize.convert(cookies.fuelmid);
-    console.log('Checking');
-    // Check in the memcache for the session
-    client.get("fuelmid_"+session[0], function(error, result)
+    // Check to see if the request is coming from our server, via PHP
+    if(addresses.indexOf(socket.request.connection.remoteAddress) >= 0)
     {
-      if (error)
-      {
-        console.log('ERROR');
-        next(new Error(error));
-      }
-      // They are authorized
-      else if (result)
-      {
-        console.log('LOGGED IN!');
         next();
-      }
-      // Not authorized
-      else
-      {
-        next(new Error('Not Authorized'));
-      }
-    });
-  }
+    }
+    else
+    {
+        // Get the cookies from the headers
+        var cookies = cookie.parse(socket.request.headers.cookie);
+        
+        var client = new memcache.Client(11211, "127.0.0.1");
+        client.connect();
+        
+        var session = unserialize.convert(cookies.fuelmid);
+        console.log('Checking');
+        // Check in the memcache for the session
+        client.get("fuelmid_"+session[0], function(error, result)
+        {
+            if (error)
+            {
+                console.log('ERROR');
+                next(new Error(error));
+            }
+            // They are authorized
+            else if (result)
+            {
+                console.log('LOGGED IN!');
+                next();
+            }
+            // Not authorized
+            else
+            {
+                console.log('Not Authorized');
+                next(new Error('Not Authorized'));
+            }
+        });
+    }
 });
 
-io.on('connection', function (socket) {
-  
-  socket.on('user_info', function (user_info)
-  {
-    // Clears the timeout for the user
-    clearTimeout(offline_timeout[user_info.id]);
-
-    // Set the users name
-    socket.id = user_info.id;
-    
-    users[user_info.id] = {
-        socket_info: socket,
-        user_info : user_info
-    };
-  });
-  
-  socket.on('append', function(user_ids, element, html, callback)
+io.on('connection', function (socket)
+{
+    socket.on('user_info', function (user_info)
     {
+        // Clears the timeout for the user
+        clearTimeout(offline_timeout[user_info.id]);
+        
+        // Set the users name
+        socket.user_id = user_info.id;
+        
+        users[user_info.id] = {
+            socket_info: socket,
+            user_info : user_info
+        };
+    });
+    
+    socket.on('append', function(data)
+    {
+        user_ids = data[0];
+        element = data[1];
+        html = data[2];
+        callback = data[3];
+        
         if (user_ids instanceof Array)
         {
             for(var user_id in user_ids)
@@ -118,30 +119,36 @@ io.on('connection', function (socket) {
         }
         else
         {
-            if (users.hasOwnProperty(user_ids))
+            if (user_ids == null)
             {
-                users[user_ids].socket_info.emit('pull', {
-                    element: element,
-                    html: html,
-                    callback : callback,
-                });
+                console.log('broadcast');
+            }
+            else
+            {
+                if (users.hasOwnProperty(user_ids))
+                {
+                    users[user_ids].socket_info.emit('pull', {
+                        element: element,
+                        html: html,
+                        callback : callback,
+                    });
+                }
             }
         }
     });
   
-  socket.on('disconnect', function ()
-  {
-    console.log('disconnected');
-    console.log(socket.id);
-    if (socket.id)
+    socket.on('disconnect', function ()
     {
-      // Make them offline after a certain point
-      offline_timeout[socket.id] = setTimeout(
-      function()
-      {
-          console.log('Deleted User');
-          delete users[socket.id]
-      }, 15000);
-    }
-  });
+        if (socket.user_id)
+        {
+            console.log('disconnected ' + socket.user_id);
+            // Make them offline after a certain point
+            offline_timeout[socket.user_id] = setTimeout(
+            function()
+            {
+                console.log('Deleted User');
+                delete users[socket.user_id]
+            }, 15000);
+        }
+    });
 });
