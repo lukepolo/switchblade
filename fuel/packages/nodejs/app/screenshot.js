9b0841c11467d451e371f57ea8ced00196407e86
Luke Policinski <lpolicinski@gmail.com>
2015-02-06 18:55:33 +0000

    Fixed issues with screenshot service deleting files, instead of creating the file just use the stream to get the data

--- fuel/packages/nodejs/app/screenshot.js
+++ fuel/packages/nodejs/app/screenshot.js
@@ -8,14 +8,18 @@ var resemble = require('node-resemble-js');
 var app = express();
 var db = mongojs.connect("mongodb://127.0.0.1:27017/switchblade_dev", ["users", "screenshots", "screenshot_images"]);
 
-var delay = 0;
+var delay = 100;
 var screenshot_folder = __dirname.replace('fuel/packages/nodejs/app','') + 'public/assets/img/screenshots/';
 
 function auth(req, res, next)
 {
-    console.log('Trying to auth '+ req.query.apikey);
-    if(req.query.apikey)
+    if (req.url === '/favicon.ico') 
     {
+	res.end();
+    } 
+    else if(req.query.apikey)
+    {
+	console.log('Trying to auth '+ req.query.apikey);
         db.users.find({api_key: req.query.apikey}, function(err, records)
         { 
             if(!err)
@@ -41,95 +45,44 @@ function auth(req, res, next)
     else
     {
         // Most likely just displaying it?
-        console.log('No API Key Sent');
         res.status(401).send('Not Authorized');
     }
 };
 
-function get_screenshot(url, options, res, api_key, user_id)
+function get_screenshot(url, options, res, api_key)
 {
+    // Start the screenshot object
     db.screenshots.insert({
 	url: url, 
-	user_id: user_id,
 	image_path: null,
 	api_key : api_key
-    }, 
-    function(err, row)
+    },
+    function(err, screenshot)
     { 
 	if(!err)
 	{
-	    var screenshot_id = row._id;
-	    var image_path = screenshot_folder + screenshot_id + '.jpg';
-
+            // Using the webshot API Libary create a stream
 	    webshot(url, options, function(err, renderStream) 
 	    {
 		if(!err)
 		{
-		    var file = fs.createWriteStream(image_path, {encoding: 'binary'});
-		    var image_data = null;;
+		    var image_data = '';
 		    renderStream.on('data', function(data) 
 		    {
-			image_data = data.toString('binary');
-
-			// Start creating the file
-			file.write(image_data, 'binary');
-
-			// Return to the browser 
-			res.write(image_data, 'binary');
+                        var chunk = data.toString('binary');
+			image_data = image_data + chunk;
+                        
+                        // Return to the browser 
+			res.write(chunk, 'binary');
 		    });
 
+                    // After the image has been completed we need to store it in our mongo db
 		    renderStream.on('end', function() 
 		    {
-			// Stop writing to file and we can save it
-			file.end();
-
 			// Stop the response
 			res.end();
-
-			fs.readFile(image_path, function (err, data)
-			{
-			    var checksum =  crypto.createHash('md5').update(data).digest('hex');
-			    db.screenshot_images.findOne({checksum: checksum}, function(err, row)
-			    {
-				if(!err)
-				{
-				    if(row == null)
-				    {
-					// Create a new record for the screenshot with the path
-					db.screenshot_images.insert({
-					    checksum: checksum,
-					    image_path: screenshot_id + '.jpg'
-					},
-					function(err, row)
-					{
-					    if(!err)
-					    {
-						// Insert is SYNC (AKA DO NOT MOVE)
-						updateScreenShot(screenshot_id, row.image_path);
-					    }
-					    else
-					    {
-						console.log('ERROR : '+ err);
-						res.status(500).send('ERROR '+ err + ', please contact support!');
-					    }
-					});
-				    }
-				    else
-				    {
-					if(screenshot_id != row.image_path+'.jpg')
-					{
-					    fs.unlink(image_path);
-					}
-					updateScreenShot(screenshot_id, row.image_path);
-				    }
-				}
-				else
-				{
-				    console.log('ERROR : '+ err);
-				    res.status(500).send('ERROR '+ err + ', please contact support!');
-				}
-			    });
-			});
+                        saveScreenShot(screenshot._id, image_data);
+                        
 		    });
 		}
 		else
@@ -187,12 +140,63 @@ app.get('/low', function(req, res)
  
 app.listen(7778);
 
+function saveScreenShot(screenshot_id, image_data)
+{
+    var checksum =  crypto.createHash('md5').update(image_data).digest('hex');
+    db.screenshot_images.findOne({checksum: checksum}, function(err, screenshot)
+    {
+        if(!err)
+        {
+            if(screenshot === null)
+            {
+                // Create a new record for the screenshot with the path
+                db.screenshot_images.insert({
+                    checksum: checksum,
+                    image_path: screenshot_id + '.jpg'
+                },
+                function(err, screenshot_image)
+                {
+                    if(!err)
+                    {
+                        // Insert is SYNC
+                        updateScreenShot(screenshot_id, screenshot_image.image_path);
 
-// Functions
+                        // TODO change extension based on what they request
+                        fs.writeFile(screenshot_folder + screenshot_id + '.jpg', image_data, 'binary', function (err) 
+                        {
+                            if (err) 
+                            {
+                                res.status(500).send('ERROR '+ err + ', please contact support!');
+                            }
+                            else
+                            {
+                                console.log('File Created ' +screenshot_id + '.jpg');
+                            }
+                        });
+                    }
+                    else
+                    {
+                        console.log('ERROR : '+ err);
+                        res.status(500).send('ERROR '+ err + ', please contact support!');
+                    }
+                });
+            }
+            else
+            {
+                updateScreenShot(screenshot_id, screenshot.image_path);
+            }
+        }
+        else
+        {
+            console.log('ERROR : '+ err);
+            res.status(500).send('ERROR '+ err + ', please contact support!');
+        }
+    });
+}
+
+// Update the screenshot to the correct image path
 function updateScreenShot(screenshot_id, image_path)
 {
-    console.log(screenshot_id);
-    console.log(image_path);
     db.screenshots.findAndModify({
 	query: {
 	    _id: screenshot_id
@@ -204,7 +208,7 @@ function updateScreenShot(screenshot_id, image_path)
 	},
 	new: true
     }, 
-    function(err, doc) 
+    function(err) 
     {
 	if(err)
 	{
