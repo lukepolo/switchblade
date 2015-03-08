9371752aaa9fa345d5663f1cc72236baf6bc4ba3
Luke Policinski <Luke@LukePOLO.com>
2015-03-08 12:03:58 -0400

    Addition of bower , and gulp with exlier

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
 var elixir = require('laravel-elixir');
-
-var bower_path = "./vendor/bower_components";
+var bower_path = './vendor/bower_components/';
 var paths = {
-  'jquery'     : bower_path + "/jquery/dist",
-  'bootstrap'  : bower_path + "/bootstrap-sass-official/assets",
-  'fontawesome': bower_path + "/fontawesome"
+    'css' : './resources/assets/css/',
+    'js' : './resources/assets/js/',
+    'jquery' : bower_path + "jquery/dist/",
+    'bootstrap' : bower_path + "bootstrap-sass-official/assets/",
+    'fontawesome' : bower_path + "fontawesome/"
 };
 
 elixir(function (mix) 
 {
     mix.sass("app.scss", "public/assets/css", {
 	includePaths: [
-	    paths.bootstrap + '/stylesheets',
-	    paths.fontawesome + '/scss'
+	    paths.css,
+	    paths.bootstrap + 'stylesheets',
+	    paths.fontawesome + 'scss'
 	]
     });
 
-    mix.scripts("app.js", "public/assets/js/app.js", "resources/assets/js");
+    // Load Jquery In
+    mix.scripts(
+	paths.jquery + 'jquery.min.js', 
+	"public/assets/js/jquery.min.js",
+	'.'
+    );
+    
     mix.scripts([
-	paths.jquery + '/jquery.min.js',
-	paths.bootstrap + '/javascripts/bootstrap.min.js'
-    ], 'public/assets/js/dependencies.js', '.');
+	// Needs to load first!
+	paths.js+ 'prettify/prettify.js',
+	paths.js+ '**',
+	paths.bootstrap + 'javascripts/bootstrap.min.js'
+    ], 'public/assets/js', '.');
 
     mix.copy(paths.fontawesome + '/fonts', 'public/assets/fonts');
 });
--- resources/views/core/private/template.blade.php
+++ resources/views/core/private/template.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
 
-        <link href="/css/app.css" rel="stylesheet">
+        <link href="/assets/css/app.css" rel="stylesheet">
 
         <!-- Fonts -->
         <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -17,7 +17,7 @@
                 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
                 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="/assets/js/jquery.min.js"></script>
     </head>
     <body>
 	@include('core/private/header')
@@ -35,7 +35,7 @@
 	    @yield('content')
 	</div>
         <!-- Scripts -->
-        <script src="/js/app.js"></script>
+        <script src="/assets/js/all.js"></script>
         <script type="text/javascript">
             $(document).ready(function()
             {
