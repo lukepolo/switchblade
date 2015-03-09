229e83f80c5539fa7cb0db0be546725bda173b45
Luke Policinski <Luke@LukePOLO.com>
2015-03-09 16:03:20 -0400

    addition of settings migration

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4cc7246b2f0c2d9e30b21faeeed06035::getLoader();
+return ComposerAutoloaderInit4b66de30a3e0254867b058f60225fb1b::getLoader();
