1bfe216d46c2bd94d986e6a2d9395511c950ab0f
Luke Policinski <Luke@LukePOLO.com>
2015-03-13 15:36:10 -0400

    Screenshot module needs rehual

--- modules/Screenshot/Http/Controllers/ScreenshotController.php
+++ modules/Screenshot/Http/Controllers/ScreenshotController.php
@@ -12,26 +12,6 @@ class ScreenshotController extends Controller {
 
     public function getDashboard()
     {
-	return View::make('screenshot::index');
-
-	echo 'convert to laravel'; die;
-        $mongodb = \Mongo_Db::instance();
-
-        $data = new \stdClass;
-
-        // Get Unique domains
-        $data->domains = $mongodb->get_where('user_domains', array(
-            'user_id' => \Auth::get_user_id()[1]
-        ));
-
-        if(count($data->domains) == 1)
-        {
-            // we can just redirect them to their view
-            \Response::Redirect(\Uri::Create('screenshot/site/view/'.$data->domains[0]['_id']->{'$id'}));
-        }
-        else
-        {
-            $this->template->content = \View::Forge('private/dashboard', $data);
-        }
+	
     }
 }
